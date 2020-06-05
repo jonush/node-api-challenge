d8fbb5ae5a945b94e689673ff905dbdbf22deddb
Jonathan Hsu <jonhsu.design@gmail.com>
2020-06-05 10:39:18 -0700

    editing actions router

--- routers/actionRouter.js
+++ routers/actionRouter.js
@@ -3,6 +3,17 @@ const express = require('express');
 const Actions = require('../data/helpers/actionModel');
 const router = express.Router();
 
+router.get('/:', (req,res) => {
+  Actions.get()
+    .then(actions => {
+      res.status(200).json(actions)
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({ message: "Could not retrieve the actions" });
+    })
+});
+
 router.get('/:id', validateActionId, (req,res) => {
   Actions.get(req.params.id)
     .then(actions => {
